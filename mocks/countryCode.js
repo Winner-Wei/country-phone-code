e427f3e19cfd3d7ec7eb931c9533c4f894ba5808
weiwei <17601068937@163.com>
2020-11-17 11:05:44 +0800

    初始化项目

---
+++ mocks/countryCode.js
@@ -0,0 +1,662 @@
+export default [{
+    "data": [{
+        "countryName": "中国",
+        "countryPinyin": "zhong guo",
+        "phoneCode": "86",
+        "countryCode": "CN"
+    }, {
+        "countryName": "中国香港",
+        "countryPinyin": "zhong guo xiang gang te bie xing zheng qu",
+        "phoneCode": "852",
+        "countryCode": "HK"
+    }, {
+        "countryName": "中国澳门",
+        "countryPinyin": "zhong guo ao men te bie xing zheng qu",
+        "phoneCode": "853",
+        "countryCode": "MO"
+    }, {
+        "countryName": "中国台湾",
+        "countryPinyin": "tai wan",
+        "phoneCode": "886",
+        "countryCode": "TW"
+    }, {
+        "countryName": "美国",
+        "countryPinyin": "mei guo",
+        "phoneCode": "1",
+        "countryCode": "US",
+        "enName": "United States of America",
+        "enFullName": "the United States of America"
+    }, {
+        "countryName": "新加坡",
+        "countryPinyin": "xin jia po",
+        "phoneCode": "65",
+        "countryCode": "SG",
+        "enName": "Singapore",
+        "enFullName": "the Republic of Singapore"
+    }, {
+        "countryName": "马来西亚",
+        "countryPinyin": "ma lai xi ya",
+        "phoneCode": "60",
+        "countryCode": "MY",
+        "enName": "Malaysia"
+    }, {
+        "countryName": "日本",
+        "countryPinyin": "ri ben",
+        "phoneCode": "81",
+        "countryCode": "JP",
+        "enName": "Japan"
+    }, {
+        "countryName": "韩国",
+        "countryPinyin": "han guo",
+        "phoneCode": "82",
+        "countryCode": "KR",
+        "enName": "Korea",
+        "enFullName": "the Republic of Korea"
+    }, {
+        "countryName": "英国",
+        "countryPinyin": "ying guo",
+        "phoneCode": "44",
+        "countryCode": "GB",
+        "enName": "United Kingdom",
+        "enFullName": "the United Kingdom of Great Britain and Northern Ireland"
+    }],
+    "key": "hot"
+}, {
+    "data": [{
+        "countryName": "阿拉伯联合酋长国",
+        "countryPinyin": "a la bo lian he qiu zhang guo",
+        "phoneCode": "971",
+        "countryCode": "AE",
+        "enName": "United Arab Emirates",
+        "enFullName": "the United Arab Emirates"
+    }, {
+        "countryName": "阿根廷",
+        "countryPinyin": "a gen ting",
+        "phoneCode": "54",
+        "countryCode": "AR",
+        "enName": "Argentina",
+        "enFullName": "The Republic of Argentina"
+    }, {
+        "countryName": "奥地利",
+        "countryPinyin": "ao de li",
+        "phoneCode": "43",
+        "countryCode": "AT",
+        "enName": "Austria",
+        "enFullName": "the Republic of Austria"
+    }, {
+        "countryName": "澳大利亚",
+        "countryPinyin": "ao da li ya",
+        "phoneCode": "61",
+        "countryCode": "AU",
+        "enName": "Australia"
+    }, {
+        "countryName": "爱沙尼亚",
+        "countryPinyin": "ai sha ni ya",
+        "phoneCode": "372",
+        "countryCode": "EE",
+        "enName": "Estonia",
+        "enFullName": "the Republic of Estonia"
+    }, {
+        "countryName": "埃塞俄比亚",
+        "countryPinyin": "ai sai e bi ya",
+        "phoneCode": "251",
+        "countryCode": "ET",
+        "enName": "Ethiopia",
+        "enFullName": "Ethiopia"
+    }, {
+        "countryName": "爱尔兰",
+        "countryPinyin": "ai er lan",
+        "phoneCode": "353",
+        "countryCode": "IE",
+        "enName": "Ireland"
+    }],
+    "key": "A"
+}, {
+    "data": [{
+        "countryName": "比利时",
+        "countryPinyin": "bi li shi",
+        "phoneCode": "32",
+        "countryCode": "BE",
+        "enName": "Belgium",
+        "enFullName": "the Kingdom of Belgium"
+    }, {
+        "countryName": "保加利亚",
+        "countryPinyin": "bao jia li ya",
+        "phoneCode": "359",
+        "countryCode": "BG",
+        "enName": "Bulgaria",
+        "enFullName": "the Republic of Bulgaria"
+    }, {
+        "countryName": "玻利维亚",
+        "countryPinyin": "bo li wei ya",
+        "phoneCode": "591",
+        "countryCode": "BO",
+        "enName": "Bolivia",
+        "enFullName": "Plurinational State of Bolivia"
+    }, {
+        "countryName": "巴西",
+        "countryPinyin": "ba xi",
+        "phoneCode": "55",
+        "countryCode": "BR",
+        "enName": "Brazil",
+        "enFullName": "the Federative Republic of Brazil"
+    }, {
+        "countryName": "白俄罗斯",
+        "countryPinyin": "bai e luo si",
+        "phoneCode": "375",
+        "countryCode": "BY",
+        "enName": "Belarus",
+        "enFullName": "the Republic of Belarus"
+    }, {
+        "countryName": "巴基斯坦",
+        "countryPinyin": "ba ji si tan",
+        "phoneCode": "92",
+        "countryCode": "PK",
+        "enName": "Pakistan",
+        "enFullName": "the Islamic Republic of Pakistan"
+    }, {
+        "countryName": "波兰",
+        "countryPinyin": "bo lan",
+        "phoneCode": "48",
+        "countryCode": "PL",
+        "enName": "Poland",
+        "enFullName": "the Republic of Poland"
+    }],
+    "key": "B"
+}, {
+    "data": [],
+    "key": "C"
+}, {
+    "data": [{
+        "countryName": "德国",
+        "countryPinyin": "de guo",
+        "phoneCode": "49",
+        "countryCode": "DE",
+        "enName": "Germany",
+        "enFullName": "he Federal Republic of Germany"
+    }, {
+        "countryName": "丹麦",
+        "countryPinyin": "dan mai",
+        "phoneCode": "45",
+        "countryCode": "DK",
+        "enName": "Denmark",
+        "enFullName": "the Kingdom of Denmark"
+    }, {
+        "countryName": "多哥",
+        "countryPinyin": "duo ge",
+        "phoneCode": "228",
+        "countryCode": "TG",
+        "enName": "Togo",
+        "enFullName": "The Republic of Togo"
+    }],
+    "key": "D"
+}, {
+    "data": [{
+        "countryName": "厄瓜多尔",
+        "countryPinyin": "e gua duo er",
+        "phoneCode": "593",
+        "countryCode": "EC",
+        "enName": "Ecuador",
+        "enFullName": "The Republic of Ecuador"
+    }, {
+        "countryName": "俄罗斯",
+        "countryPinyin": "e luo si",
+        "phoneCode": "7",
+        "countryCode": "RU",
+        "enName": "Russian Federation",
+        "enFullName": "the Russian Federation"
+    }],
+    "key": "E"
+}, {
+    "data": [{
+        "countryName": "芬兰",
+        "countryPinyin": "fen lan",
+        "phoneCode": "358",
+        "countryCode": "FI",
+        "enName": "Finland",
+        "enFullName": "the Republic of Finland"
+    }, {
+        "countryName": "法国",
+        "countryPinyin": "fa guo",
+        "phoneCode": "33",
+        "countryCode": "FR",
+        "enName": "France",
+        "enFullName": "the French Republic"
+    }, {
+        "countryName": "菲律宾",
+        "countryPinyin": "fei lu bin",
+        "phoneCode": "63",
+        "countryCode": "PH",
+        "enName": "Philippines",
+        "enFullName": "the Republic of the Philippines"
+    }],
+    "key": "F"
+}, {
+    "data": [{
+        "countryName": "刚果（金）",
+        "countryPinyin": "gang guo （jin）",
+        "phoneCode": "243",
+        "countryCode": "CD",
+        "enName": "Congo",
+        "enFullName": "The Democratic Republic of the Congo"
+    }, {
+        "countryName": "哥伦比亚",
+        "countryPinyin": "ge lun bi ya",
+        "phoneCode": "57",
+        "countryCode": "CO",
+        "enName": "Columbia",
+        "enFullName": "The Republic of Colombia"
+    }],
+    "key": "G"
+}, {
+    "data": [{
+        "countryName": "韩国",
+        "countryPinyin": "han guo",
+        "phoneCode": "82",
+        "countryCode": "KR",
+        "enName": "Korea",
+        "enFullName": "the Republic of Korea"
+    }, {
+        "countryName": "荷兰",
+        "countryPinyin": "he lan",
+        "phoneCode": "31",
+        "countryCode": "NL",
+        "enName": "Netherlands",
+        "enFullName": "the Kingdom of the Netherlands"
+    }],
+    "key": "H"
+}, {
+    "data": [{
+        "countryName": "加拿大",
+        "countryPinyin": "jia na da",
+        "phoneCode": "1",
+        "countryCode": "CA",
+        "enName": "Canada"
+    }, {
+        "countryName": "捷克共和国",
+        "countryPinyin": "jie ke gong he guo",
+        "phoneCode": "420",
+        "countryCode": "CZ",
+        "enName": "Czech Republic",
+        "enFullName": "the Czech Republic"
+    }, {
+        "countryName": "加纳",
+        "countryPinyin": "jia na",
+        "phoneCode": "233",
+        "countryCode": "GH",
+        "enName": "Ghana",
+        "enFullName": "The Republic of Ghana"
+    }, {
+        "countryName": "柬埔寨",
+        "countryPinyin": "jian bu zhai",
+        "phoneCode": "855",
+        "countryCode": "KH",
+        "enName": "Cambodia",
+        "enFullName": "the Kingdom of Cambodia"
+    }],
+    "key": "J"
+}, {
+    "data": [{
+        "countryName": "喀麦隆",
+        "countryPinyin": "ka mai long",
+        "phoneCode": "237",
+        "countryCode": "CM",
+        "enName": "Cameroon",
+        "enFullName": "The Republic of Cameroon"
+    }, {
+        "countryName": "克罗地亚",
+        "countryPinyin": "ke luo de ya",
+        "phoneCode": "385",
+        "countryCode": "HR",
+        "enName": "Croatia",
+        "enFullName": "the Republic of Croatia"
+    }],
+    "key": "K"
+}, {
+    "data": [{
+        "countryName": "立陶宛",
+        "countryPinyin": "li tao wan",
+        "phoneCode": "370",
+        "countryCode": "LT",
+        "enName": "Lithuania",
+        "enFullName": "the Republic of Lithuania"
+    }, {
+        "countryName": "卢森堡",
+        "countryPinyin": "lu sen bao",
+        "phoneCode": "352",
+        "countryCode": "LU",
+        "enName": "Luxembourg",
+        "enFullName": "the Grand Duchy of Luxembourg"
+    }, {
+        "countryName": "拉脱维亚",
+        "countryPinyin": "la tuo wei ya",
+        "phoneCode": "371",
+        "countryCode": "LV",
+        "enName": "Latvia",
+        "enFullName": "the Republic of Latvia"
+    }, {
+        "countryName": "罗马尼亚",
+        "countryPinyin": "luo ma ni ya",
+        "phoneCode": "40",
+        "countryCode": "RO",
+        "enName": "Romania"
+    }],
+    "key": "L"
+}, {
+    "data": [{
+        "countryName": "孟加拉国",
+        "countryPinyin": "meng jia la guo",
+        "phoneCode": "880",
+        "countryCode": "BD",
+        "enName": "Bangladesh",
+        "enFullName": "the People's Republic of Bangladesh"
+    }, {
+        "countryName": "马里",
+        "countryPinyin": "ma li",
+        "phoneCode": "223",
+        "countryCode": "ML",
+        "enName": "Mali",
+        "enFullName": "The Republic of Mali"
+    }, {
+        "countryName": "缅甸",
+        "countryPinyin": "mian dian",
+        "phoneCode": "95",
+        "countryCode": "MM",
+        "enName": "Myanmar",
+        "enFullName": "the Union of Myanmar"
+    }, {
+        "countryName": "蒙古",
+        "countryPinyin": "meng gu",
+        "phoneCode": "976",
+        "countryCode": "MN",
+        "enName": "Mongolia"
+    }, {
+        "countryName": "马耳他",
+        "countryPinyin": "ma er ta",
+        "phoneCode": "356",
+        "countryCode": "MT",
+        "enName": "Malta",
+        "enFullName": "the Republic of Malta"
+    }, {
+        "countryName": "墨西哥",
+        "countryPinyin": "mo xi ge",
+        "phoneCode": "52",
+        "countryCode": "MX",
+        "enName": "Mexico",
+        "enFullName": "the United Mexican States"
+    }, {
+        "countryName": "马来西亚",
+        "countryPinyin": "ma lai xi ya",
+        "phoneCode": "60",
+        "countryCode": "MY",
+        "enName": "Malaysia"
+    }, {
+        "countryName": "秘鲁",
+        "countryPinyin": "mi lu",
+        "phoneCode": "51",
+        "countryCode": "PE",
+        "enName": "Peru",
+        "enFullName": "The Republic of Peru"
+    }, {
+        "countryName": "美国",
+        "countryPinyin": "mei guo",
+        "phoneCode": "1",
+        "countryCode": "US",
+        "enName": "United States of America",
+        "enFullName": "the United States of America"
+    }],
+    "key": "M"
+}, {
+    "data": [{
+        "countryName": "尼日利亚",
+        "countryPinyin": "ni ri li ya",
+        "phoneCode": "234",
+        "countryCode": "NG",
+        "enName": "Nigeria",
+        "enFullName": "Federal Republic of Nigeria"
+    }, {
+        "countryName": "挪威",
+        "countryPinyin": "nuo wei",
+        "phoneCode": "47",
+        "countryCode": "NO",
+        "enName": "Norway",
+        "enFullName": "the Kingdom of Norway"
+    }, {
+        "countryName": "尼泊尔",
+        "countryPinyin": "ni po er",
+        "phoneCode": "977",
+        "countryCode": "NP",
+        "enName": "Nepal"
+    }],
+    "key": "N"
+}, {
+    "data": [{
+        "countryName": "葡萄牙",
+        "countryPinyin": "pu tao ya",
+        "phoneCode": "351",
+        "countryCode": "PT",
+        "enName": "Portugal",
+        "enFullName": "the Portuguese Republic"
+    }],
+    "key": "P"
+}, {
+    "data": [{
+        "countryName": "瑞士",
+        "countryPinyin": "rui shi",
+        "phoneCode": "41",
+        "countryCode": "CH",
+        "enName": "Switzerland",
+        "enFullName": "the Swiss Confederation"
+    }, {
+        "countryName": "日本",
+        "countryPinyin": "ri ben",
+        "phoneCode": "81",
+        "countryCode": "JP",
+        "enName": "Japan"
+    }, {
+        "countryName": "瑞典",
+        "countryPinyin": "rui dian",
+        "phoneCode": "46",
+        "countryCode": "SE",
+        "enName": "Sweden",
+        "enFullName": "the Kingdom of Sweden"
+    }],
+    "key": "R"
+}, {
+    "data": [{
+        "countryName": "塞浦路斯",
+        "countryPinyin": "sai pu lu si",
+        "phoneCode": "537",
+        "countryCode": "CY",
+        "enName": "Cyprus",
+        "enFullName": "the Republic of Cyprus"
+    }, {
+        "countryName": "斯里兰卡",
+        "countryPinyin": "si li lan ka",
+        "phoneCode": "94",
+        "countryCode": "LK",
+        "enName": "Sri Lanka",
+        "enFullName": "the Democratic Socialist Republic of Sri Lanka"
+    }, {
+        "countryName": "沙特阿拉伯",
+        "countryPinyin": "sha te a la bo",
+        "phoneCode": "966",
+        "countryCode": "SA",
+        "enName": "Saudi Arabia",
+        "enFullName": "the Kingdom of Saudi Arabia"
+    }, {
+        "countryName": "斯洛文尼亚",
+        "countryPinyin": "si luo wen ni ya",
+        "phoneCode": "386",
+        "countryCode": "SI",
+        "enName": "Slovenia",
+        "enFullName": "the Republic of Slovenia"
+    }, {
+        "countryName": "斯洛伐克",
+        "countryPinyin": "si luo fa ke",
+        "phoneCode": "421",
+        "countryCode": "SK",
+        "enName": "Slovakia",
+        "enFullName": "the Slovak Republic"
+    }, {
+        "countryName": "苏里南",
+        "countryPinyin": "su li nan",
+        "phoneCode": "597",
+        "countryCode": "SR",
+        "enName": "Suriname",
+        "enFullName": "The Republic of Suriname"
+    }],
+    "key": "S"
+}, {
+    "data": [{
+        "countryName": "泰国",
+        "countryPinyin": "tai guo",
+        "phoneCode": "66",
+        "countryCode": "TH",
+        "enName": "Thailand",
+        "enFullName": "the Kingdom of Thailand"
+    }, {
+        "countryName": "台湾",
+        "countryPinyin": "tai wan",
+        "phoneCode": "886",
+        "countryCode": "TW"
+    }, {
+        "countryName": "坦桑尼亚",
+        "countryPinyin": "tan sang ni ya",
+        "phoneCode": "255",
+        "countryCode": "TZ",
+        "enName": "Tanzania",
+        "enFullName": "The United Republic of Tanzania"
+    }],
+    "key": "T"
+}, {
+    "data": [{
+        "countryName": "文莱",
+        "countryPinyin": "wen lai",
+        "phoneCode": "673",
+        "countryCode": "BN",
+        "enName": "Brunei Darussalam"
+    }, {
+        "countryName": "乌干达",
+        "countryPinyin": "wu gan da",
+        "phoneCode": "256",
+        "countryCode": "UG",
+        "enName": "Uganda",
+        "enFullName": "The Republic of Uganda"
+    }, {
+        "countryName": "乌拉圭",
+        "countryPinyin": "wu la gui",
+        "phoneCode": "598",
+        "countryCode": "UY",
+        "enName": "Uruguay",
+        "enFullName": "The Oriental Republic of Uruguay"
+    }, {
+        "countryName": "委内瑞拉",
+        "countryPinyin": "wei nei rui la",
+        "phoneCode": "58",
+        "countryCode": "VE",
+        "enName": "Venezuela",
+        "enFullName": "Bolivarian Republic of Venezuela"
+    }],
+    "key": "W"
+}, {
+    "data": [{
+        "countryName": "西班牙",
+        "countryPinyin": "xi ban ya",
+        "phoneCode": "34",
+        "countryCode": "ES",
+        "enName": "Spain",
+        "enFullName": "the Kingdom of Spain"
+    }, {
+        "countryName": "希腊",
+        "countryPinyin": "xi la",
+        "phoneCode": "30",
+        "countryCode": "GR",
+        "enName": "Greece",
+        "enFullName": "the Hellenic Republic"
+    }, {
+        "countryName": "匈牙利",
+        "countryPinyin": "xiong ya li",
+        "phoneCode": "36",
+        "countryCode": "HU",
+        "enName": "Hungary",
+        "enFullName": "the Republic of Hungary"
+    }, {
+        "countryName": "新西兰",
+        "countryPinyin": "xin xi lan",
+        "phoneCode": "64",
+        "countryCode": "NZ",
+        "enName": "New Zealand"
+    }, {
+        "countryName": "新加坡",
+        "countryPinyin": "xin jia po",
+        "phoneCode": "65",
+        "countryCode": "SG",
+        "enName": "Singapore",
+        "enFullName": "the Republic of Singapore"
+    }],
+    "key": "X"
+}, {
+    "data": [{
+        "countryName": "英国",
+        "countryPinyin": "ying guo",
+        "phoneCode": "44",
+        "countryCode": "GB",
+        "enName": "United Kingdom",
+        "enFullName": "the United Kingdom of Great Britain and Northern Ireland"
+    }, {
+        "countryName": "印度尼西亚",
+        "countryPinyin": "yin du ni xi ya",
+        "phoneCode": "62",
+        "countryCode": "ID",
+        "enName": "Indonesia",
+        "enFullName": "the Republic of Indonesia"
+    }, {
+        "countryName": "印度",
+        "countryPinyin": "yin du",
+        "phoneCode": "91",
+        "countryCode": "IN",
+        "enName": "India",
+        "enFullName": "the Republic of India"
+    }, {
+        "countryName": "意大利",
+        "countryPinyin": "yi da li",
+        "phoneCode": "39",
+        "countryCode": "IT",
+        "enName": "Italy",
+        "enFullName": "the Republic of Italy"
+    }, {
+        "countryName": "越南",
+        "countryPinyin": "yue nan",
+        "phoneCode": "84",
+        "countryCode": "VN",
+        "enName": "Viet Nam",
+        "enFullName": "the Socialist Republic of Viet Nam"
+    }],
+    "key": "Y"
+}, {
+    "data": [{
+        "countryName": "智利",
+        "countryPinyin": "zhi li",
+        "phoneCode": "56",
+        "countryCode": "CL",
+        "enName": "Chile",
+        "enFullName": "the Republic of Chile"
+    }, {
+        "countryName": "中国",
+        "countryPinyin": "zhong guo",
+        "phoneCode": "86",
+        "countryCode": "CN"
+    }, {
+        "countryName": "中国香港",
+        "countryPinyin": "zhong guo xiang gang te bie xing zheng qu",
+        "phoneCode": "852",
+        "countryCode": "HK"
+    }, {
+        "countryName": "中国澳门",
+        "countryPinyin": "zhong guo ao men te bie xing zheng qu",
+        "phoneCode": "853",
+        "countryCode": "MO"
+    }],
+    "key": "Z"
+}]
